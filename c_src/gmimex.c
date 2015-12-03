107af4284ab5ee3735f8dcbcc62b6d7a644461bd
Dejan Strbac <dejan.strbac@gmail.com>
2015-12-03 17:25:56 +0100

    don't load images

--- c_src/gmimex.c
+++ c_src/gmimex.c
@@ -645,9 +645,8 @@ static GString *build_attributes(GumboNode *node, GumboAttribute *at, gboolean n
   if (node->type == GUMBO_NODE_ELEMENT) {
     if ((node->v.element.tag == GUMBO_TAG_IMG) && !g_ascii_strcasecmp(at->name, "src"))
       g_string_append(atts, "data-");
-  } else {
-    g_string_append(atts, at->name);
   }
+  g_string_append(atts, at->name);
 
   // how do we want to handle attributes with empty values
   // <input type="checkbox" checked />  or <input type="checkbox" checked="" />
